125c68ad061205d1689ddd8e28cbacb46a87fb93
BeshoyGorgi <beshoygorgi10@gamil.com>
2025-11-08 14:07:52 +0100

    Website Test URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "faithpoints-production.up.railway.app"; 
+export const API_BASE_URL = "https://faithpoints-production.up.railway.app"; 
 
 // https://faith-points.onrender.com 
 
