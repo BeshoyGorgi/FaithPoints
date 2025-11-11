b4b2a44dddc5757abe9b9a33cb42ad50a8f8a85f
BeshoyGorgi <beshoygorgi10@gamil.com>
2025-11-11 11:38:25 +0100

    Website URL geändert

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
-export const API_BASE_URL = "https://faithpoints-production.up.railway.app/"; 
+export const API_BASE_URL = "https://faith-points.onrender.com"; 
 
-// https://faith-points.onrender.com 
+// https://faithpoints-production.up.railway.app/ 
 
 //Wenn ich local arbeiten möchte http://localhost:3000
 // Und in .env auch bei DB_HOST ---> localhost
