9c24827aeec97f5ba99dd4c273e3a6a9d80a5326
BeshoyGorgi <beshoygorgi10@gamil.com>
2026-02-06 00:17:33 +0100

    Fix css3

--- frontend/main/script.js
+++ frontend/main/script.js
@@ -630,6 +630,6 @@ document.getElementById("screenshot").addEventListener("click", () => {
 
 // ====== Placements ======
 document.getElementById("goPlacements")?.addEventListener("click", () => {
-  window.location.href = "/platzierungen/platzierungen.html";
+  window.location.href = "/placements/placements.html";
 });
 
